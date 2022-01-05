47c846bbe6aaf22dbc9791e9a27a7aa3f2e54c4c
Felipe Justus Matos <70666131+FelipeJustusMatos@users.noreply.github.com>
2022-01-05 16:12:55 -0300

    Add files via upload

---
+++ lib/data/dummy_rolls.dart
@@ -0,0 +1,27 @@
+import 'package:rolls/modelo/rolls.dart';
+
+const Map<String, Rolls> DUMMY_ROLLS = {
+  '1': const Rolls(
+    id: '1',
+    name: 'Cortina',  
+  ),
+
+  '2': const Rolls(
+    id: '2',
+    name: 'Cobertor',
+    ),
+
+  '3': const Rolls(
+    id: '3',
+    name: 'Toalha',
+    ),
+
+  '4': const Rolls(
+    id: '4',
+    name: 'Fronha',
+    ),
+  '5': const Rolls(
+    id: '5',
+    name: 'Lençol',
+    ),
+};
